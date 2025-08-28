e54e88fde34c03c5615e983c354ff84320b58fb2
vishvas2005 <156111892+vishvas2005@users.noreply.github.com>
2025-08-28 21:15:36 +0530

    finalfortoday

--- src/Components/project/Parts.jsx
+++ src/Components/project/Parts.jsx
@@ -8,7 +8,7 @@ function Parts() {
     </div>
     <div className='h-fit w-[50vw] ml-[45vw]  rounded-xl overflow-hidden mt-20'>
     <img src="./warriror.png" alt="" />
-      <a href="https://vishvas2005.github.io/warriros/"><h1 className='flex gap-3 underline hover:cursor-pointer text-[25px] lg:text-[50px] text-white absolute -ml-[20vw] lg:-ml-[0vw] lg:ml-[53vw] mt-[5vw] lg:mt-[0vw] lg:-mt-[27vw] uppercase'>Warriors <span className=" pt-[1vh] lg:pt-4 text-[18px] lg:text-[37px]  -rotate-[30deg]"><FaArrowRightLong /></span></h1></a>
+      <a href="https://vishvas2005.github.io/warriros/"><h1 className='flex gap-3 underline hover:cursor-pointer text-[25px] lg:text-[50px] text-white absolute -ml-[20vw] lg:-ml-[0vw] lg:ml-[33vw] mt-[5vw] lg:mt-[0vw] lg:-mt-[27vw] uppercase'>Warriors <span className=" pt-[1vh] lg:pt-4 text-[18px] lg:text-[37px]  -rotate-[30deg]"><FaArrowRightLong /></span></h1></a>
     </div>
     <div className='h-fit w-[50vw] ml-[10vw] lg:ml-20 mt-20 rounded-xl overflow-hidden'>
      <img src="./pro.png" alt="" />
