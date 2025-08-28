ca99be6fc955dd47ef90c74a7c4294749bdad66d
vishvas2005 <156111892+vishvas2005@users.noreply.github.com>
2025-08-28 21:02:57 +0530

    fonti

--- src/Components/project/Parts.jsx
+++ src/Components/project/Parts.jsx
@@ -7,21 +7,11 @@ function Parts() {
       <a href="https://vishvas2005.github.io/projects/"><h1 className='flex gap-3 underline hover:cursor-pointer text-[25px] lg:text-[50px] text-white absolute ml-[35vw] lg:ml-[53vw] mt-[5vw] lg:mt-[0vw] lg:-mt-[27vw] uppercase'> sunshine<span className=" pt-[1vh] lg:pt-4 text-[18px] lg:text-[37px]  -rotate-[30deg]"><FaArrowRightLong /></span></h1></a>
     </div>
     <div className='h-fit w-[50vw] ml-[45vw]  rounded-xl overflow-hidden mt-20'>
-    <video
-        src=".\src\public\videos\service.mp4"
-        autoPlay
-        loop
-        muted
-      ></video>
+    <img src="./warriror.png" alt="" />
       <a href="https://vishvas2005.github.io/warriros/"><h1 className='flex gap-3 underline hover:cursor-pointer text-[25px] lg:text-[50px] text-white absolute -ml-[20vw] lg:-ml-[0vw] lg:ml-[53vw] mt-[5vw] lg:mt-[0vw] lg:-mt-[27vw] uppercase'>Warriors <span className=" pt-[1vh] lg:pt-4 text-[18px] lg:text-[37px]  -rotate-[30deg]"><FaArrowRightLong /></span></h1></a>
     </div>
     <div className='h-fit w-[50vw] ml-[10vw] lg:ml-20 mt-20 rounded-xl overflow-hidden'>
-     <video className='w-[60vw]'
-        src=".\src\public\videos\port (online-video-cutter.com) (1).mp4"
-        autoPlay
-        loop
-        muted
-      ></video>
+     <img src="./pro.png" alt="" />
       <a href="https://vishvas2005.github.io/vishvas/"><h1 className='flex gap-3 underline hover:cursor-pointer text-[25px] lg:text-[50px] text-white absolute ml-[35vw] lg:ml-[53vw] mt-[5vw] lg:mt-[0vw] lg:-mt-[27vw] uppercase'> PORtfolio<span className="  pt-[1vh] lg:pt-4 text-[18px] lg:text-[37px]  -rotate-[30deg]"><FaArrowRightLong /></span></h1></a>
     </div>
     </>
